f1c7cb9aa8584326644fd53ff02ef70a5dbf8dab
Pedro Amui <curiamuiph@gmail.com>
2018-12-19 15:18:58 -0800

    Read Encoder + Expo Drive
    Co-Authored-By: Caden Hewlett <cadenh139@edu.sd45.bc.ca>

--- D33p Space 2019/ProtoBase/src/main/java/frc/robot/commands/arcadeDrive.java
+++ D33p Space 2019/ProtoBase/src/main/java/frc/robot/commands/arcadeDrive.java
@@ -10,32 +10,32 @@ package frc.robot.commands;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-
+import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class arcadeDrive extends Command {
 	//exponential variables
-	private final int JoyDead = 8;
+	private final double JoyDead = 0.25;
 	private final double MotorMin = 0.3;
 	private final double DriveExp = 1.5;
 	//exponential function
-	int exponential(double joystickVal, double driveExp, int joyDead, double motorMin){
+	int exponential(double joystickVal, double driveExp, double joyDead, double motorMin){
 		int joySign;
-		    double joyMax = 128 - joyDead;
+		    double joyMax = 1 - joyDead;
 		    double joyLive = Math.abs(joystickVal) - joyDead;
 		    if (joystickVal > 0) {joySign = 1;}
 		    else if (joystickVal < 0) {joySign = -1;}
 		    else {joySign = 0;}
-		    int power = (int) (joySign * (motorMin + ((128 - motorMin) * (Math.pow(joyLive, driveExp) / Math.pow(joyMax, driveExp)))));
+		    int power = (int) (joySign * (motorMin + ((1 - motorMin) * (Math.pow(joyLive, driveExp) / Math.pow(joyMax, driveExp)))));
 		    return power;
 	}
-	double joyYval = exponential(Robot.oi.main.getY(Hand.kLeft), DriveExp, JoyDead, MotorMin);
-	double joyXval = exponential(Robot.oi.main.getX(Hand.kRight), DriveExp, JoyDead, MotorMin); //works -- why?
-	/*double joyYval = OI.main.getY();*/ //doesn't work-- why?
 
-	private final Joystick m_stick = new Joystick(RobotMap.LEFT_JOYSTICK.value);
+
+	double joyYval = Robot.oi.main.getY(Hand.kLeft);
+	double joyXval = Robot.oi.main.getX(Hand.kRight); //works -- why?
+
 	public arcadeDrive() { // Called when initialize arcadeDrive
 		requires(Robot.drivebase);
 	}
@@ -46,13 +46,15 @@ public class arcadeDrive extends Command {
 
 	protected void execute() { // Run periodically as command goes
 		//exponential Xbox joy x and joy y
-		joyYval = exponential(Robot.oi.main.getY(Hand.kLeft), DriveExp, JoyDead, MotorMin);
-		joyXval = exponential(Robot.oi.main.getX(Hand.kRight), DriveExp, JoyDead, MotorMin);
-		Robot.drivebase.set(ControlMode.PercentOutput, (joyYval + joyXval), (joyYval - joyXval));
-		//original drivebase
-		Robot.drivebase.set(ControlMode.PercentOutput, m_stick.getY() + m_stick.getX(), m_stick.getY() - m_stick.getX());
-	}
+		joyYval = (Math.abs(Robot.oi.main.getY(Hand.kLeft)) > JoyDead ? Robot.oi.main.getY(Hand.kLeft) : 0);
+		joyXval = (Math.abs(Robot.oi.main.getX(Hand.kRight)) > JoyDead ? Robot.oi.main.getX(Hand.kRight) : 0);
+		Robot.drivebase.set(ControlMode.PercentOutput, (joyYval + joyXval), (joyYval - joyXval)); //arcade drive, mode is percent output
+		SmartDashboard.putNumber("Left Position", (Robot.drivebase.getLeftPosition()));
+		SmartDashboard.putNumber("Joystick Left", joyYval);
+		SmartDashboard.putNumber("Joystick Right", joyXval);
 
+		
+	}
 	protected boolean isFinished() { // Tell if it's finished
 		return false;
 	}
--- D33p Space 2019/ProtoBase/src/main/java/frc/robot/subsystems/driveBase.java
+++ D33p Space 2019/ProtoBase/src/main/java/frc/robot/subsystems/driveBase.java
@@ -15,7 +15,6 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
@@ -42,9 +41,7 @@ public class driveBase extends Subsystem implements PIDOutput {
 		rightMotor = new TalonSRX(RobotMap.RIGHT_MOTOR.value);
 		ahrs = new AHRS(I2C.Port.kMXP);
 		
-		//Read Encoder Absolute
-		//int pulseWidthPos = leftMotor.getSensorCollection().getPulseWidthPosition();
-
+		
 		Robot.initVictor(leftMotorF, true);
 		Robot.initTalon(leftMotor, true);
 		Robot.initVictor(rightMotorF, false);
@@ -72,8 +69,16 @@ public class driveBase extends Subsystem implements PIDOutput {
 		turnController.enable();
 	}
 
+	public double getLeftPosition() {
+		return -((leftMotor.getSensorCollection().getPulseWidthPosition()/4096)*1.57);
+	}
+
+	public int getRightPosition(){
+		return rightMotor.getSensorCollection().getPulseWidthPosition();
+	}
+
 	public void set(ControlMode mode, double rightValue, double leftValue) {
-		leftMotor.set(mode, leftValue);
+		leftMotor.set(mode, leftValue); 
 		rightMotor.set(mode, rightValue);
 	}
 
