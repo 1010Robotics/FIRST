e81582b02e6fc009d3202852d7b07945dc8e1a7a
cadenhewlett <cadenh139@edu.sd45.bc.ca>
2018-12-19 12:28:03 -0800

    Attempt at an exponential function
    Either the exponential function goes into the command or the subsystem, or it doesn't matter which. I'm not sure. It's in the command for now.  Also, I don't know what the constant integers should be for the joyMax differential, so I've set it to 128 for now.
    
    Co-Authored-By: Pedro Amui <curiamuiph@gmail.com>

--- D33p Space 2019/ProtoBase/src/main/java/frc/robot/commands/arcadeDrive.java
+++ D33p Space 2019/ProtoBase/src/main/java/frc/robot/commands/arcadeDrive.java
@@ -15,9 +15,23 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class arcadeDrive extends Command {
-
-	double joyYval = Robot.oi.main.getY();
-	double joyXVal = Robot.oi.main.getX(); //works -- why?
+	//exponential variables
+	private final int JoyDead = 8;
+	private final double MotorMin = 0.3;
+	private final double DriveExp = 1.5;
+	//exponential function
+	int exponential(double joystickVal, double driveExp, int joyDead, double motorMin){
+		int joySign;
+		    double joyMax = 128 - joyDead;
+		    double joyLive = Math.abs(joystickVal) - joyDead;
+		    if (joystickVal > 0) {joySign = 1;}
+		    else if (joystickVal < 0) {joySign = -1;}
+		    else {joySign = 0;}
+		    int power = (int) (joySign * (motorMin + ((128 - motorMin) * (Math.pow(joyLive, driveExp) / Math.pow(joyMax, driveExp)))));
+		    return power;
+	}
+	double joyYval = exponential(Robot.oi.main.getY(), DriveExp, JoyDead, MotorMin);
+	double joyXval = exponential(Robot.oi.main.getX(), DriveExp, JoyDead, MotorMin); //works -- why?
 	/*double joyYval = OI.main.getY();*/ //doesn't work-- why?
 
 	private final Joystick m_stick = new Joystick(RobotMap.LEFT_JOYSTICK.value);
@@ -30,6 +44,11 @@ public class arcadeDrive extends Command {
 	}
 
 	protected void execute() { // Run periodically as command goes
+		//exponential Xbox joy x and joy y
+		joyYval = exponential(Robot.oi.main.getY(), DriveExp, JoyDead, MotorMin);
+		joyXval = exponential(Robot.oi.main.getX(), DriveExp, JoyDead, MotorMin);
+		Robot.drivebase.set(ControlMode.PercentOutput, (joyYval + joyXval), (joyYval - joyXval));
+		//original drivebase
 		Robot.drivebase.set(ControlMode.PercentOutput, m_stick.getY() + m_stick.getX(), m_stick.getY() - m_stick.getX());
 	}
 
