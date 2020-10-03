d5144ca185a4600469f625059652e1bd7b28f267
IrisYuYang <66691895+IrisYuYang@users.noreply.github.com>
2020-10-03 15:36:37 -0700

    2019 frc code

---
+++ robot/commands/teleopSolenoid.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.*;
+
+public class teleopSolenoid extends Command {
+
+  public teleopSolenoid() {
+    requires(Robot.solenoid);
+  }
+
+  protected void initialize() {
+    Robot.solenoid.startCompressor();
+  }
+
+  protected void execute() {
+    if(Robot.oi.main.getTriggerAxis(Hand.kLeft) > 0.1){
+			Robot.solenoid.extendSolenoid();
+		}
+    
+    else{
+      Robot.solenoid.disableSolenoid();
+    }
+    
+    SmartDashboard.putString("Solenoid State", Robot.solenoid.actuatorState.toString());
+  }
+
+  protected boolean isFinished() {
+    return false;
+  }
+
+  protected void end() {
+    Robot.solenoid.disableSolenoid();
+  }
+
+  protected void interrupted() {
+    end();
+  }
+  
+}
---
+++ robot/subsystems/limeLight.java
@@ -0,0 +1,74 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class limeLight extends Subsystem {
+
+	private static NetworkTableInstance table = null;
+
+	public enum LightMode {
+		eOn, eOff, eBlink
+	}
+
+	public enum CameraMode {
+		eVision, eDriver
+	}
+
+	public boolean isTarget() {
+		return getValue("tv").getDouble(0) == 1;
+	}
+
+	public double getTx() {
+		return getValue("tx").getDouble(0.00);
+	}
+
+	public double getTy() {
+		return getValue("ty").getDouble(0.00);
+	}
+
+	public double getTa() {
+		return getValue("ta").getDouble(0.00);
+	}
+
+	public double getTs() {
+		return getValue("ts").getDouble(0.00);
+	}
+
+	public double getTl() {
+		return getValue("tl").getDouble(0.00);
+	}
+
+	public void setLedMode(LightMode mode) {
+		getValue("ledMode").setNumber(mode.ordinal());
+	}
+
+	public void setCameraMode(CameraMode mode) {
+		getValue("camMode").setNumber(mode.ordinal());
+	}
+
+	public void setPipeline(int number) {
+		getValue("pipeline").setNumber(number);
+	}
+
+	private static NetworkTableEntry getValue(String key) {
+		if (table == null) {
+			table = NetworkTableInstance.getDefault();
+		}
+
+		return table.getTable("limelight-tenton").getEntry(key);
+  	}
+
+  	@Override
+ 	 public void initDefaultCommand() {
+ 	}
+  
+}
